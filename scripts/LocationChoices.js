25b16f3aecbb328392ab71d1015986a9e3bcb6fd
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-13 13:09:17 -0500

    Updated LocationChoices module

---
+++ scripts/LocationChoices.js
@@ -0,0 +1,18 @@
+export const locationChoices = async() => {
+    //Fetches locations from the API
+    const response = await fetch("http://localhost:8088/socioLocations")
+    const locations = await response.json()
+
+    let html = `
+        <div class="survey-input">
+            <h2>What type of area do you live in?</h2>
+    `
+
+    //TODO: Generate radio buttons and add to html
+
+    html += `
+        </div>
+    `
+
+    return html
+}

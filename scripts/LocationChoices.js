2331fb257055b5676b8b6b281c431ae936dd5a05
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-14 10:32:40 -0500

    Updated LocationChoices module

--- scripts/LocationChoices.js
+++ scripts/LocationChoices.js
@@ -1,11 +1,18 @@
 import { setSocioLocation } from "./TransientState.js"
 
+const handleLocationChange = (changeEvent) => {
+    if(changeEvent.target.name === "location") {
+        const locationId = parseInt(changeEvent.target.value)
+        setSocioLocation(locationId)
+    }
+}
+
 export const LocationChoices = async() => {
     //Fetches locations from the API
     const response = await fetch("http://localhost:8088/socioLocations")
     const locations = await response.json()
-
-
+    
+    document.addEventListener("change", handleLocationChange)
 
     let html = `
         <div class="survey-input">

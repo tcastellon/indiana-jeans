87524ea2670916a56aeaca5e0a6bed0ab78729d5
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-14 09:33:38 -0500

    Updated JeanChoices module

--- scripts/JeanChoices.js
+++ scripts/JeanChoices.js
@@ -1,10 +1,20 @@
+import { setOwnsBlueJeans } from "./TransientState.js"
+
+const handleOwnershipChange = (changeEvent) => {
+    if(changeEvent.target.name === "ownsJeans") {
+        const convertedToBoolean = JSON.parse(changeEvent.target.value)
+        setOwnsBlueJeans(convertedToBoolean)
+    }
+}
+
 export const JeanChoices = () => {
+    document.addEventListener("change", handleOwnershipChange)
+    
     let html = `
         <div class="survey-input">
             <h2>Do you own a pair of blue jeans?</h2>
             <input type="radio" name="ownsJeans" value="true" /> Yes
             <input type="radio" name="ownsJeans" value="false" /> No
-            <input type="radio" name="ownsJeans" value="false" /> Maybe
         </div>
     `
 

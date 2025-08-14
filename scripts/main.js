b230fc5932108da2d6380f9924500c688dd27dbd
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-08-14 10:51:17 -0500

    Updated Main module

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 import { JeanChoices } from "./JeanChoices.js"
 import { LocationChoices } from "./LocationChoices.js"
+import { SubmissionButton } from "./SubmissionButton.js"
 
 const container = document.querySelector("#container")
 
 const render = async() => {
     const jeansHTML = JeanChoices()
     const locationsHTML = await LocationChoices()
-    
+    const buttonHTML = SubmissionButton()
+
     container.innerHTML = `
     ${jeansHTML}
     ${locationsHTML}
+    ${buttonHTML}
     `
 }
 
